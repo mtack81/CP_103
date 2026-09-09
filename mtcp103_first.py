10302269b3423e45a21d9ab8bdb51a29f0708fce
mtack81 <mtack81@live.com>
2026-09-09 13:56:08 -0400

    add first Python assignment

---
+++ mtcp103_first.py
@@ -0,0 +1,12 @@
+my_name = 'Mike Tackett'
+my_hobby = 'Culinary Arts'
+my_birth_year = 1981
+current_year = 2026
+favorite_food ='Grilled Pineapple Chili Duck'
+
+print('My name is' ,my_name)
+print( 'My hobby and current skills are in' ,my_hobby)
+age = current_year-my_birth_year
+print('I am about', age, 'years old.')
+print('My favorite food is', favorite_food)
+
