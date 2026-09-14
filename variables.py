4485f17bacd1713f295e8d8ffe26ff1b3a3fe92c
mtack81 <mtack81@live.com>
2026-09-14 12:32:37 -0400

    add python assignment from linux Mint git push

---
+++ variables.py
@@ -0,0 +1,26 @@
+
+print( 1 + 2 )
+
+a = 3
+print ( a )
+
+a = 4
+b = 5
+print ( a + b )
+
+
+a = 6
+b = 7
+print( 'a + b =' , a + b )
+
+
+a = 8
+b = 9
+print( a , '+' , b , '=' , a + b )
+
+
+a = 1
+b = 2
+c = a
+d = b
+print( c , d )
