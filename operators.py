1aed0aaf8add3de9d4b964409f94b880a79bf30d
mtack81 <mtack81@live.com>
2026-09-14 12:48:09 -0400

    another assignment

---
+++ operators.py
@@ -0,0 +1,35 @@
+
+# String plus Sting
+# Concatination !!
+a = 'cat'
+b = 'fish'
+print( a + b )
+
+
+# Int plus Int
+a = 1
+b = 2
+print ( a + b )
+
+
+# Int times Int
+a = 3
+b = 4
+print ( a * b )
+
+
+# Int plus Float
+a = 5
+b = 6.0
+print ( a + b )
+
+# Int times String
+a = 7
+b = '8.0'
+print ( a * b )
+
+
+# Int plus String
+a = 9
+b = '10'
+print ( a + b ) # ERROR !! You cannot add strings to numbers
