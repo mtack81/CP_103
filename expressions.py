4dc35240466b33960045516fdf299756b2d2c2bf
mtack81 <mtack81@live.com>
2026-09-14 12:41:06 -0400

    expression py assignment

---
+++ expressions.py
@@ -0,0 +1,20 @@
+
+
+x = 'Apples'
+print(  x , 'is type', type( x ) )
+
+
+x = 1
+print(  x , 'is type', type( x ) )
+
+
+x = 2.0
+print(  x , 'is type', type( x ) )
+
+
+x = '3'
+print(  x , 'is type', type( x ) )
+
+
+x = '4.0'
+print(  x , 'is type', type( x ) )
