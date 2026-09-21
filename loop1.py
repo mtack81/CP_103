f3b96c756dfbe4917a50ce3b85cd4443b1a4ae55
mtack81 <mtack81@live.com>
2026-09-21 12:39:41 -0400

    single loops for python

---
+++ loop1.py
@@ -0,0 +1,5 @@
+# Loop 1
+for i in range( 6 ):
+    print( i , end = ' , ' )
+print('')
+# TEST FOR ERRORS BEFORE MOVING ON
---
+++ loops.py
@@ -0,0 +1,21 @@
+# Loop 1
+for i in range( 6 ): #defaults to start at 0 and go by 1
+    print( i , end = ' , ' )
+print('')
+
+for n in range( 1 , 11 ):
+    print( n , end = ' , ' )
+print('')
+
+for x in range( 15 , 60 , 15 ):
+    print( x , end = ' , ' )
+print('')
+
+for x in range( 3 , -1 , -1 ):
+    print( x , end = ' , ' )
+print('')
+
+for name in [ 'Anna' , 'Bobby' , 'Clay' , 'Dan']:
+    print( name , end = ' and ' )
+print('')
+
